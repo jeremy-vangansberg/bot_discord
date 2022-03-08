59791c02717f1f46699744cf0188a04a1f5c9f60
jerem <jeremy.vangansberg@gmail.com>
2022-03-08 10:45:52 +0100

    .env

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from discord.ext import tasks
 import time
 import os
 
+# local version
 # from dotenv import load_dotenv
 # load_dotenv()
 
@@ -10,25 +11,22 @@ import os
 # ID_CHANNEL = int(os.getenv('ID_CHANNEL'))
 # TOKEN =os.getenv('TOKEN')
 
+# Heroku version
 ID_SERVER = int(os.environ['ID_SERVER'])
 ID_CHANNEL = int(os.environ['ID_CHANNEL'])
 TOKEN = os.environ['TOKEN']
-
-print(type(ID_SERVER), type(ID_CHANNEL), type(TOKEN))
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(type(ID_SERVER), type(ID_CHANNEL), type(TOKEN))
     mytask.start()
 
 @tasks.loop(seconds=60)#1 min
 async def mytask():
-    print(type(ID_SERVER), type(ID_CHANNEL), type(TOKEN))
     channel = client.get_guild(ID_SERVER).get_channel(ID_CHANNEL)
     strings = time.strftime("%H,%M")
-    if strings in ["10,43","11,00","14,30","16,00","17:02"]:
+    if strings in ["09,48","11,00","14,30","16,00","17:02"]:
         await channel.send("Rappel : n'oubliez pas de signer sur SWS <@&913805722511355965>")
         
 client.run(TOKEN)
