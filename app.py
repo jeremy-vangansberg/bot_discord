1d0d133e19c736bd778c2384a023d364c7cce447
Sof1ane <43276871+Sof1ane@users.noreply.github.com>
2022-06-15 10:36:00 +0200

    a

--- app.py
+++ app.py
@@ -1,35 +1,26 @@
-import discord
-from discord.ext import tasks 
 import time
 import os
 import datetime
+import requests
+from discord import Webhook, RequestsWebhookAdapter, Embed
 
 # local version
 # from dotenv import load_dotenv
 # load_dotenv()
 
-# ID_SERVER = int(os.getenv('ID_SERVER'))
-# ID_CHANNEL = int(os.getenv('ID_CHANNEL'))
-# TOKEN =os.getenv('TOKEN')
+# DISCORD_WEBHOOK = os.environ['DISCORD_WEBHOOK']
 
 # Heroku version
-ID_SERVER = int(os.environ['ID_SERVER'])
-ID_CHANNEL = int(os.environ['ID_CHANNEL'])
-TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
+DISCORD_WEBHOOK = os.environ['DISCORD_WEBHOOK']
 
-@client.event
-async def on_ready():
-    mytask.start()
-
-@tasks.loop(seconds=60)#1 min
-async def mytask():
-    channel = client.get_guild(ID_SERVER).get_channel(ID_CHANNEL)
+def loop():
     strings = time.strftime("%H:%M")
     today = datetime.date.today().isoweekday()
     if today in [1,2,3,4,5] :
         if strings in ["09:00","10:00","13:30","15:00"] :
-            await channel.send("Rappel : n'oubliez pas de signer sur SWS <@&913805722511355965>")       
+            webhook = Webhook.from_url(discord_webhook, adapter=RequestsWebhookAdapter())
+            webhook.send("Rappel : n'oubliez pas de signer sur SWS <@&913805722511355965>")      
 
-client.run(TOKEN)
+loop() 
+
