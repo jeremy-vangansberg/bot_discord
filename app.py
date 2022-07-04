b3fd473bf591e3d32f6f5ba3852394d7710983c3
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-07-04 11:11:51 +0200

    up hours test

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def loop():
     strings = time.strftime("%H:%M")
     today = datetime.date.today().isoweekday()
     if today in [1,2,3,4,5] :
-        if strings in ["09:00","09:05","10:00","13:30","15:00"] :
+        if strings in ["09:00","09:14","10:00","13:30","15:00"] :
             webhook = Webhook.from_url(DISCORD_WEBHOOK, adapter=RequestsWebhookAdapter())
-            webhook.send("Rappel : n'oubliez pas de signer sur SWS <@&913805722511355965>")
+            webhook.send(content="Rappel : n'oubliez pas de signer sur SWS <@&913805722511355965>")
 
 loop() 
 
