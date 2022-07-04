d98c9e72007f7c915157ea0770009b1fd81a76c9
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-07-04 12:06:54 +0200

    erreur

--- app.py
+++ app.py
@@ -18,9 +18,6 @@ def loop():
     strings = time.strftime("%H:%M")
     today = datetime.date.today().isoweekday()
     if today in [1,2,3,4,5] :
-        if strings in ["09:00","09:14","10:00","13:30","15:00"] :
-            webhook = Webhook.from_url(DISCORD_WEBHOOK, adapter=RequestsWebhookAdapter())
-            webhook.send(content="Rappel : n'oubliez pas de signer sur SWS <@&913805722511355965>")
-
-loop() 
+        if strings in ["08:00","90:00","12:30","14:00"] :
+            await channel.send("Rappel : n'oubliez pas de signer sur SWS <@&913805722511355965>")       
 
