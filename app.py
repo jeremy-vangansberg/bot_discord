d5cc59b62e78509ed58e536d672a8813f71e4845
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-07-22 15:56:25 +0200

    pytz manager

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import time
-import os
 import datetime
-import requests
-from discord import Webhook, RequestsWebhookAdapter, Embed
+from discord import Webhook, RequestsWebhookAdapter
+import pytz
 
 # local version
 # from dotenv import load_dotenv
@@ -16,19 +15,24 @@ DISCORD_WEBHOOK = "https://discord.com/api/webhooks/993440182185504804/QUY7fkpaL
 
 
 def loop():
-    strings = time.strftime("%H:%M")
-    today = datetime.date.today().isoweekday()
+    paris = pytz.timezone('Europe/Paris')
+    dt = datetime.datetime.now()
     dls = time.localtime().tm_isdst
-    hours_summer = ["08:00", "09:00", "13:00", "14:00"]
-    hours_winter = ["09:00", "10:00", "14:00", "15:00"]
-    if today in [1, 2, 3, 4, 5]:
-        # Vérification si heure d'été ou heure d'hiver
-        if dls:
-            hours = hours_summer
-        else:
-            hours = hours_winter
+    if dls:
+        loc_dt1 = paris.localize(dt, is_dst=True)
+    else:
+        loc_dt1 = paris.localize(dt, is_dst=False)
 
-        if strings in hours:
+    hour = str(loc_dt1.hour)
+    minute = str(loc_dt1.minute)
+    weekday = str(loc_dt1.weekday()+1)
+
+    hour_to_test = "{}:{}".format(hour, minute)
+
+    hours_to_push = ["10:00", "11:00", "15:00", "16:00"]
+
+    if weekday in ['1', '2', '3', '4', '5']:
+        if hour_to_test in hours_to_push:
             webhook = Webhook.from_url(
                 DISCORD_WEBHOOK, adapter=RequestsWebhookAdapter())
             webhook.send(
